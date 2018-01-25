029614ab3b60b090f39993b8250948063052c7a3
aziz58 <dataiolib@gmail.com>
2018-01-25 23:09:31 +0100

    ajouter les taches a faire de la class etudiant et base de donnee

--- DataBase/DataBaseClass.java
+++ DataBase/DataBaseClass.java
@@ -1,5 +1,17 @@
 package DataBase;
 
-public class DataBaseClass {
+import java.sql.Connection;
+import java.util.ArrayList;
 
+public class DataBaseClass {
+	/* ajouter les methodes nécissaite pour la connexion au base de donnée et 
+	 * la creation des tableau si'il n'existe pas
+	 *
+	 */
+	public ArrayList<Etudiant> getAllStudiants(){
+		return null;
+	}
+	public ArrayList<Etudiant> getStudNearToDate(int nombreDesJours){
+		
+	}
 }
--- DataBase/Etudiant.java
+++ DataBase/Etudiant.java
@@ -1,5 +1,22 @@
 package DataBase;
 
-public class Etudiant {
+import java.util.Date;
 
+public class Etudiant {
+	String nom , prenom ;
+	Date dateDebut , dateFin;
+	//je ne sait pas quelle class on définit pour une date je mit Date et toi chercher ce qu'il faut 
+	//sinon mit String 
+	
+	//constructeur
+	
+	public String getCreateTableQuery(){
+		return "";
+		
+	}/*create table .... */
+	
+	public String getInsertIntoQuery(){
+		return "";
+	}/*insert Into Query ...*/
+	
 }
